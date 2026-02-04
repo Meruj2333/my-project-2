dc8cf8144ec0f3dc867764439f2b0e518cdd5117
Meruj2333 <merujharutyunyan0620@gmail.com>
2026-02-05 02:41:17 +0400

    added login logic

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,22 @@
+console.log()
+
+
+
 import 'dotenv/config';
-import express from 'express';
+import express, {json} from 'express';
 import cors from 'cors';
 import session from "express-session";
+import { readFile, writeFile } from "fs/promises";
+import bcrypt from "bcrypt";
+
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:63342",
+    credentials: true
+}));
 app.use(
     session({
         secret: 'My session',
@@ -22,52 +32,60 @@ let users = [
     {id:1,login:'mery@gmail.ru',password:'1234',role:'admin'}
 
 ];
+app.post('/login', async (req,res)=>{
+    const {email,password} = req.body;
 
+
+    const fileData = await readFile('users.json','utf-8');
+    let data = JSON.parse(fileData);
+
+    let user = false;
+    if (data[email]) {
+         user = await bcrypt.compare(password ,data[email].password);
+    }
+
+    if(user){
+        req.session.user = user;
+        return res.json(user)
+    }
+
+    res.status(401).json({message:"Invalid login"})
+})
 app.get('/', (req, res) => {
-    if (req.session.user.role == 'admin') {
+    if (req.session.user && req.session.user.role === 'admin') {
         res.json(users);
-
-    }else{
+    } else {
         res.json({message:'not admin'});
     }
 });
 
-app.get('/login', (req, res) => {
-    const {login,password} = req.query;
-   let usersValues=users.find(user=>user.login.toLowerCase()===login.toLowerCase() && user.password===password);
-   if(usersValues){
-       let newUser = {
-           ...usersValues
-       }
-       delete newUser.password;
-       req.session.user = newUser;
-       return res.json(newUser);
-   }else{
-       res.json({message:'Login not found'});
-   }
 
 
-})
-app.post('/register', (req, res) => {
-    const {name,email,login,password} = req.body;
-    let val={id:Date.now(), name,email,login,password,role:'user'}
-    users.push(val)
-    let newValue={
-        ...val
+
+app.post('/register', async (req, res) => {
+    const {name,email,password,isAdmin} = req.body;
+    if(!name || !email || !password) {
+        return res.status(400).json({message:'Fill all fields'});
     }
-    delete newValue.password;
-    req.session.user = {
-        newValue
-    };
+    const heashesPassword = await bcrypt.hash(password,10);
 
-res.json(users);
+    let val={id:Date.now(), name,email,password:heashesPassword,role:isAdmin?'admin':'user'};
 
+    let data = {};
+    try {
+        const fileData = await readFile('users.json','utf-8');
+        data = JSON.parse(fileData);
+    }catch(err) {
+        data = {}
+    }
 
-})
-
-
-
-
+    data[val.email] = val;
+    await writeFile('users.json', JSON.stringify(data, null, 2));
+    const safeUser = {...val};
+    delete safeUser.password;
+    req.session.user = safeUser;
+    res.json(safeUser);
+});
 
 
 
@@ -91,11 +109,30 @@ app.get('/logout', (req, res) => {
 })
 
 
+app.get('/admin', (req, res) => {
+    if(req.session.user && req.session.user.role === 'admin'){
+        return res.json(users);
+    }
+    res.status(403).json({message:'Access denied'});
+})
+
+
+
 
 
 
 const PORT = process.env.PORT || 3001;
 
+
+app.get('/check/:psw',async (req, res) => {
+    const {psw} = req.params;
+    const isMatch = await bcrypt.compare(psw,'$2b$10$KnAX47bceEaaQkaJDnxwnO8rZeI6YBpj2x6TkIOfOyGxRPtULpmA.')
+    if(isMatch){
+        return res.json("chisht email or pass")
+
+    }
+    res.json({message:'Access denied'});
+})
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
--- frontend/script.js
+++ frontend/script.js
@@ -1,16 +1,102 @@
 const  $=s=>document.querySelector(s)
-fetch('http://localhost:3001/').then(res=>res.json()).then(data=>{
+fetch('http://localhost:3001/profile', {
+    credentials: 'include'
+}).then(res=>res.json()).then(data=>{
+    if(data.message){
+        return
+    }
     console.table(data);
+    $('#guestArea').classList.add('d-none');
+    $('#appArea').classList.remove('d-none');
+    if(data.newValue.role==='admin'){
+        $('#adminTabBtn').classList.remove('d-none');
+    }
+
+
+
 })
 
-$('#save').onclick=()=>{
-    let fieldEl=$('#field')
-    fetch('http://localhost:3001/add',{
+$('#regBtn').onclick=(e)=>{
+    e.preventDefault()
+
+    let regName=$('#regName')
+    let regEmail=$('#regEmail')
+    let regPassword=$('#regPassword')
+    let regPassword2=$('#regPassword2')
+    let regIsAdmin=$('#regIsAdmin')
+if(!regName.value ||!regEmail.value||!regPassword.value||!regPassword2.value||regPassword.value.length<6||regPassword.value!==regPassword2.value){
+    alert("sxal")
+    return false
+}
+
+
+
+    fetch('http://localhost:3001/register',{
         method: 'POST',
-        body: JSON.stringify({value:fieldEl.value}),
-        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({name:regName.value,email:regEmail.value,password:regPassword.value,isAdmin:regIsAdmin.checked}),
+        headers: {'Content-Type': 'application/json',
+            'Accept': 'application/json',
+
+        },
+        credentials: 'include',
 
     }).then(res=>{
-        console.log(res)
+        if(res.ok){
+            return res.json()
+        }
+        throw res.error
+    }).then(data=>{
+        $('#guestArea').classList.add('d-none');
+        $('#appArea').classList.remove('d-none');    })
+}
+
+
+
+$('#btnLogout').onclick=()=>{
+    fetch('http://localhost:3001/logout', {
+        credentials: 'include'
+    }).then(res=>res.json()).then(data=>{
+        console.table(data);
+        $('#guestArea').classList.remove('d-none');
+        $('#appArea').classList.add('d-none');
+
     })
 }
+
+$('#adminTabBtn').onclick=()=>{
+    fetch('http://localhost:3001/admin', {
+        credentials: 'include'
+    }).then(res=>res.json()).then(data=>{
+        console.table(data);
+
+
+    })
+}
+
+$('#loginBtn').onclick = (e) => {
+    e.preventDefault();
+
+    const email = $('#loginEmail').value;
+    const password = $('#loginPassword').value;
+
+    fetch('http://localhost:3001/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        credentials: 'include',
+        body: JSON.stringify({ email, password })
+    })
+        .then(res => res.json())
+        .then(data => {
+            if(data.message){
+                alert(data.message);
+                return;
+            }
+
+            alert("Login Successful ✅");
+
+            $('#guestArea').classList.add('d-none');
+            $('#appArea').classList.remove('d-none');
+        })
+        .catch(err => console.log(err));
+};
+
